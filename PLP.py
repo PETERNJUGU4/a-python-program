aec9f1f7926aede9c99351c8dad62a89fd49d0b1
Peter Murage <njugu.peter54@gmail.com>
2024-11-07 13:20:09 +0300

    python program

---
+++ PLP.py
@@ -0,0 +1,9 @@
+# user_input.py
+
+# Use the input() function to ask for user details
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
